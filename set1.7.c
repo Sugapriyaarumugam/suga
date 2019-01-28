c203f001a382112f3507c779a79c7389db2856a7
Sugapriyaarumugam <46921407+Sugapriyaarumugam@users.noreply.github.com>
2019-01-28 16:11:06 +0530

    set1.7.c

---
+++ set1.7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+ char a[100]="hello";
+ int i, n;
+
+ scanf("%d",&n);
+ for(i=0;i<=n;i++)
+ {
+     
+     printf("%s",a);
+     printf("\n");
+ }
+    return 0;
+}
