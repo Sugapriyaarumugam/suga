834ab406f03f6695904b79d15ec7d1a7dbfda4a0
Sugapriyaarumugam <46921407+Sugapriyaarumugam@users.noreply.github.com>
2019-01-29 22:03:22 +0530

    Create set1.9.c

---
+++ set1.9.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+
+int i,n,k,a[50],sum=0;
+scanf("%d",&n);
+scanf("%d",&k);
+ for(i=0;i<n;i++)
+ {
+   scanf("%d",&a[i]);
+ }
+  for(i=k;i<=k;i++)
+  {
+  sum=sum+a[i];
+  }
+  printf("%d",sum);
+
+getch();
+}
