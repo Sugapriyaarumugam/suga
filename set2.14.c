7cc80a9679386104af419fbd00dfcbc73d944a8a
Sugapriyaarumugam <46921407+Sugapriyaarumugam@users.noreply.github.com>
2019-02-08 22:24:07 +0530

    set2.14.c

---
+++ set2.14.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+  int N,Q,i=0;
+  scanf("%d %d",&N,&Q);
+  if(N,Q<=10000)
+  {
+    for(i=N+1;i<Q;i++)
+    {
+      if(i%2!=0)
+      {
+        printf("%d ",i);
+      }
+    }
+  }
+}
