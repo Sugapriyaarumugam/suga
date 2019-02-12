27705c3b9844f4be84b3961f609de90b721069dc
Sugapriyaarumugam <46921407+Sugapriyaarumugam@users.noreply.github.com>
2019-02-12 09:17:35 +0530

    set3.21.c

---
+++ set3.21.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+  int N,A,D,t;
+  scanf("%d %d %d",&N,&A,&D);
+  if(1>=N,A,D<=100000)
+  {
+      t=(N*(2*A+(N-1)*D))/2;
+      printf("%d",t);
+  }
+  return 0;
+}
