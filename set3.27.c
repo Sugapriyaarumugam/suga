509c1f54faaffa2c36a3ee1cffc5cfd4530c43ff
Sugapriyaarumugam <46921407+Sugapriyaarumugam@users.noreply.github.com>
2019-02-18 15:57:11 +0530

    Create set3.27.c

---
+++ set3.27.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    char s[1000];
+    int i,l;
+    scanf("%s",&s);
+    l=strlen(s);
+    for(i=0;i<=l;i++)
+    {
+        if(s[i]>='1'&&s[i]<='9')
+        {
+            printf("yes");
+            break;
+        }
+else
+{
+    printf("no");
+    break;
+}
+    }
+    
+   
+
+}
