78ab1cf34728d0cabb320090bd16c3e4908556c9
Sugapriyaarumugam <46921407+Sugapriyaarumugam@users.noreply.github.com>
2019-02-26 16:08:04 +0530

    Create set5.50.c

---
+++ set5.50.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main() 
+{
+	int n,flag=0,temp;
+	scanf("%d",&n);
+	temp=n;
+	while(temp!=1)
+	{
+		if(temp%2!=0)
+		{
+			flag=1;
+			break;
+		}
+		temp=temp/2;
+	}
+	if(flag==0)
+	{
+		printf("yes");
+	}
+	else if(flag==1)
+	{
+		printf("no");
+	}
+	return 0;
+}
