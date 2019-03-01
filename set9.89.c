182818b4b9ad74acf2f40dc370a6a1bf3a3bb64f
Sugapriyaarumugam <46921407+Sugapriyaarumugam@users.noreply.github.com>
2019-03-01 13:08:54 +0530

    Create set9.89.c

---
+++ set9.89.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+#include<string.h>
+int main(void) {
+	char a[100];
+	int n,i;
+	scanf("%s",a);
+	n=strlen(a);
+	for(i=n-1;i>=0;i--)
+	{
+		printf("%c",a[i]);
+	}
+	
+	return 0;
+}
