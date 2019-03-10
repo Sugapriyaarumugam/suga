685a5a920e44284ac3fb5cc605cf7f2d23371cc2
Sugapriyaarumugam <46921407+Sugapriyaarumugam@users.noreply.github.com>
2019-03-10 12:32:56 +0530

    Create p79.c

---
+++ p79.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+    int n,a[100],i,d=0,r,j,temp=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]>a[j])
+            {
+             d=a[i]-a[j];
+            }
+            else
+            {
+                d=a[j]-a[i];
+            }
+            if(d>temp)
+            {
+                temp=d;
+            }
+        }
+    }
+        printf("%d",temp);
+}
