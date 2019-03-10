3946988fa00cb79ec2e71ded46c5757e0a129bb5
Sugapriyaarumugam <46921407+Sugapriyaarumugam@users.noreply.github.com>
2019-03-10 19:56:40 +0530

    Create p17.c

---
+++ p17.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int n,m,i;
+scanf("%d %d",&n,&m);
+for(i=1;;i++)
+{
+    if(i%n==0&&i%m==0)
+    {
+        
+        printf("%d",i);
+        break;
+    }   
+}
+}
