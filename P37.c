21fc82991f7c83fdaaea57ca48f5bda08489c54e
Sugapriyaarumugam <46921407+Sugapriyaarumugam@users.noreply.github.com>
2019-03-11 07:44:42 +0530

    Create P37.c

---
+++ P37.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int num1,num2,a,i,count=0;
+scanf("%d",&a);
+for(i=0;i<a;i++)
+{
+    scanf("%d %d",&num1,&num2);
+    if(num1<num2)
+    {
+        count++;
+    }
+}
+printf("%d",count);
+}
