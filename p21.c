b74e17a2e6f86bdce75b1f7720625d7cff797940
Sugapriyaarumugam <46921407+Sugapriyaarumugam@users.noreply.github.com>
2019-03-11 07:10:48 +0530

    Create p21.c

---
+++ p21.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int a1,b1,a2,b2,n1,n2;
+    scanf("%d %d",&a1,&b1);
+     scanf("%d %d",&a2,&b2);
+      scanf("%d %d",&n1,&n2);
+      if((a1=0 &&a2==0 &&n1==0) ||(b1==0&& b2==0 && n2==0))
+      {
+          printf("yes");
+      }
+      else
+      {
+          printf("no");
+      }
+}
